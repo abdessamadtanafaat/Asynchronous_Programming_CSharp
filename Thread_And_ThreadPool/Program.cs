d89145efa04e066225067ca56b52ffaf556e145e
tanafaat <tanafaatabdessamad@gmail.com>
2024-11-13 00:36:14 +0100

    Thread_and_threadPool

---
+++ Thread_And_ThreadPool/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+
+namespace Asynchronous_Programming_CSharp;
+
+public class Program
+{
+    private static readonly WeatherService _weatherService = new WeatherService();
+
+    static void Main(String[] args)
+    {
+        int threadCount = 10; // number of concurrent requests. 
+        
+        Console.WriteLine("Using Thread:");
+        MeasurePerformanceUsingThreads(threadCount); 
+        
+        Console.WriteLine("\n Using ThreadPool: ");
+        MeasurePerformanceUsingThreadsPool(threadCount); 
+    }
+
+    private static void MeasurePerformanceUsingThreads(int threadCount)
+    {
+        Thread[] threads = new Thread[threadCount];
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            threads[i] = new Thread(FetchData); 
+            threads[i].Start();
+        }
+        
+        foreach (Thread thread in threads) {
+            thread.Join(); 
+        }
+        
+        stopwatch.Stop();
+        Console.WriteLine($"Total Time (Thread): {stopwatch.ElapsedMilliseconds} ms");
+    }
+
+    private static void MeasurePerformanceUsingThreadsPool(int threadCount)
+    {
+        CountdownEvent countdown = new CountdownEvent(threadCount);
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                FetchData();
+                countdown.Signal();
+            });
+        }
+
+        countdown.Wait();
+        stopwatch.Stop(); 
+        Console.WriteLine($"Total Time (ThreadPool): {stopwatch.ElapsedMilliseconds} ms");
+    }
+
+    private static void FetchData()
+    {
+        var data = _weatherService.FetchWeatherData(); 
+        Console.WriteLine("Data fetched successfully.");
+    }
+}
---
+++ Thread_And_ThreadPool/WeatherService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+namespace Asynchronous_Programming_CSharp;
+
+public class WeatherService
+{
+    private readonly string _apiUrl = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+    public string FetchWeatherData()
+    {
+        var request = (HttpWebRequest)WebRequest.Create(_apiUrl);
+        request.Method = "GET";
+
+        using (var response = (HttpWebResponse)request.GetResponse())
+        {
+            using (var stream = response.GetResponseStream())
+            {
+                if (stream == null) return "NO DATA REVEIVED";
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd(); 
+                }
+            }
+        }
+    }
+}
